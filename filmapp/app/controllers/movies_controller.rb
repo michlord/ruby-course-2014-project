a27c8e6f039fd0f4a9c37476a954f72477094290
Michał Zimniak <zimniak.michal@gmail.com>
2014-12-28 00:00:59 +0100

    Created an incomplete view for viewing a list of movies and editing them.

--- filmapp/app/controllers/movies_controller.rb
+++ filmapp/app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show]
+  before_action :set_movie, only: [:show, :edit, :update]
+  
+  def index
+    @movies = Movie.all
+  end
   
   def show
     @genres_list = @movie.genres
@@ -9,8 +13,24 @@ class MoviesController < ApplicationController
     @release_dates = @movie.release_dates
   end
   
+  def edit
+    
+  end
+  
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie), notice: 'Product was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+  
   private
     def set_movie
       @movie = Movie.find(params[:id])
+    end
+    
+    def movie_params
+      params.require(:movie).permit(:title, :description, :webpage, :runtime, :poster)
     end
 end
