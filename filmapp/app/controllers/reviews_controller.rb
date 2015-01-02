def09a83046bfd7d3c68b8b7c651766daf4c87b8
Michał Zimniak <zimniak.michal@gmail.com>
2015-01-02 16:31:40 +0100

    Users can now write reviews for a movie.

--- filmapp/app/controllers/reviews_controller.rb
+++ filmapp/app/controllers/reviews_controller.rb
@@ -1,13 +1,23 @@
 class ReviewsController < ApplicationController
+  before_filter :authenticate_user!, only: [:create, :new]
   def page
     @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews.paginate(page: params[:page], per_page: 5)
   end
   
   def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build()
   end
 
   def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build(params.require(:review).permit(:headline, :content).merge(user: current_user))
+    if @review.save
+      redirect_to movie_path(@movie), notice: "Review was successfully created."
+    else
+      render action: "new"
+    end
   end
 
   def show
--- filmapp/app/models/review.rb
+++ filmapp/app/models/review.rb
@@ -5,4 +5,6 @@ class Review < ActiveRecord::Base
     rand_id = rand Review.count
     Review.where("id >= ?", rand_id).first
   end
+  validates :headline, presence: true
+  validates :content, presence: true
 end
